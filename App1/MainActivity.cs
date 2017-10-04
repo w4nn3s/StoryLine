6e65f25d1d96713320ed49817f0d8c3edc957209
W4nn3s <Wannes@pottendeirm.be>
2017-10-04 08:56:48 +0200

    new file:   .gitignore 	new file:   App.sln 	new file:   App1/App.csproj 	new file:   App1/Assets/AboutAssets.txt 	new file:   App1/GettingStarted.Xamarin 	new file:   App1/MainActivity.cs 	new file:   App1/Properties/AndroidManifest.xml 	new file:   App1/Properties/AssemblyInfo.cs 	new file:   App1/Resources/AboutResources.txt 	new file:   App1/Resources/Resource.Designer.cs 	new file:   App1/Resources/drawable/ln1 	new file:   App1/Resources/drawable/ln2 	new file:   App1/Resources/drawable/ln3 	new file:   App1/Resources/drawable/ln4 	new file:   App1/Resources/drawable/lncover 	new file:   App1/Resources/drawable/mncover 	new file:   App1/Resources/layout/Main.axml 	new file:   App1/Resources/layout/ViewAlbum.axml 	new file:   App1/Resources/values/Strings.xml 	new file:   App1/ViewAlbumActivity.cs

---
+++ App1/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Net;
+using Android.Content;
+
+namespace App1
+{
+    [Activity(Label = "StoryLine", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            ImageButton imageButton1 = FindViewById<ImageButton>(Resource.Id.imageButton1);
+
+          /*  imageButton1.Click += (sender, e) =>
+            {
+                var intent = new Intent(this, typeof(ViewAlbumActivity));
+                intent.put
+
+            };*/
+        }
+    }
+}
+
